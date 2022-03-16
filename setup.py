e255a2024caea971aff2b1f39f98e419fde8fa4c
mohammedterryjack <67234383+mohammedterryjack@users.noreply.github.com>
2022-03-16 17:10:55 +0000

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = '0.0.2'
+VERSION = '0.0.3'
 DESCRIPTION = 'FFAST: Fast Fourier Analysis for Sentence embeddings and Tokenisation'
 LONG_DESCRIPTION = 'Fast and lightweight NLP pipeline for ML tasks: powerful tokeniser and (model-free) sentence embeddings using Fast Fourier transforms, power means and Wordnet'
 
@@ -16,7 +16,7 @@ setup(
     install_requires=['nltk', 'jellyfish', 'Unidecode', 'numpy', 'scipy'],
     keywords=['python', 'embedding', 'tokenisation', 'fast fourier', 'nlp', 'nlu'],
     classifiers=[
-        "Development Status :: 1 - Planning",
+        "Development Status :: 4 - Beta",
         "Intended Audience :: Developers",
         "Programming Language :: Python :: 3",
         "Operating System :: Unix",
