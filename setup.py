1c98acf2943bcd415eb9cbcf08aee48907f96fa7
mohammedterryjack <67234383+mohammedterryjack@users.noreply.github.com>
2022-03-16 20:54:36 +0000

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = '0.0.3'
+VERSION = '0.0.4'
 DESCRIPTION = 'FFAST: Fast Fourier Analysis for Sentence embeddings and Tokenisation'
 LONG_DESCRIPTION = 'Fast and lightweight NLP pipeline for ML tasks: powerful tokeniser and (model-free) sentence embeddings using Fast Fourier transforms, power means and Wordnet'
 
