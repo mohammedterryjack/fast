0dc36ce6eb4eb9064e62a24e37d637015adf5f85
mohammedterryjack <67234383+mohammedterryjack@users.noreply.github.com>
2022-03-18 19:09:12 +0000

    Add files via upload

--- ffast/poincare/utils.py
+++ ffast/poincare/utils.py
@@ -1,26 +1,14 @@
 from enum import Enum
-from os import getcwd
-from os.path import exists 
-from requests import get
+from pathlib import Path 
 
 from numpy import loadtxt
 
 from ffast.preprocessor import PreprocessingPipeline
 
-def download_file(url:str,path:str) -> None:
-    if exists(path):
-        return
-    result = get(url)
-    if result.ok:
-        with open(path,'w') as vector_file:
-            vector_file.write(result.text)
-
 PREPROCESSOR = PreprocessingPipeline()
 METAPHONES = "ABCEFHIJKLMNOPRSTUWXY0. "
 SIZE_METAPHONES = len(METAPHONES)
-HERE = getcwd()
-PATH = f"{HERE}/poincare.txt"
-download_file(path=PATH,url="https://raw.githubusercontent.com/mohammedterryjack/Joint-Intent-Slots/master/word_vectors/poincare.txt")
+PATH = Path(__file__).parent/"poincare.txt"
 raw_vocab = loadtxt(PATH,usecols=0,dtype=str)
 VOCABULARY = list(map(PREPROCESSOR.normalise,raw_vocab))
 VECTORS = loadtxt(PATH,usecols=range(1,101))
