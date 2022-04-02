126d7b2f0a4c2a69f1f5b1d008b92fc7b99820e9
mohammedterryjack <67234383+mohammedterryjack@users.noreply.github.com>
2022-04-02 15:34:04 +0100

    Add files via upload

--- ffast/__init__.py
+++ ffast/__init__.py
@@ -1,13 +1,10 @@
-__version__ = "0.3.3"
+__version__ = "0.3.5"
 WORDNET = "wordnet"
 POINCARE = "poincare"
 
 def load(vectors:str=WORDNET):
     if vectors==WORDNET:
         from ffast.wordnet.tokeniser import Tokeniser
-        from nltk import download
-        download(WORDNET)
-        download('stopwords')
         return Tokeniser()
     if vectors==POINCARE:
         from ffast.poincare.tokeniser import Tokeniser
